f9a744dcba9bd50247a6caffdbb65e22e33ea447
npollock14 <33988166+npollock14@users.noreply.github.com>
2019-10-24 19:46:08 -0700

    Initial Commit

---
+++ Costco Racer/src/CostcoRacerMain.java
@@ -0,0 +1,150 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class CostcoRacerMain extends JPanel
+		implements ActionListener, KeyListener, MouseListener, MouseMotionListener, MouseWheelListener {
+	private static final long serialVersionUID = 1L;
+
+	int screenWidth = 1000;
+	int screenHeight = 1000;
+	boolean[] keys = new boolean[300];
+	boolean[] keysToggled = new boolean[300];
+	boolean[] mouse = new boolean[200];
+
+	// ============== end of settings ==================
+
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+
+	}
+
+	public void update() throws InterruptedException {
+		
+	}
+
+	private void init() {
+		
+	}
+
+	// ==================code above ===========================
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+		try {
+			update();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		repaint();
+	}
+
+	public static void main(String[] arg) {
+		@SuppressWarnings("unused")
+		CostcoRacerMain d = new CostcoRacerMain();
+	}
+
+	public CostcoRacerMain() {
+		JFrame f = new JFrame();
+		f.setTitle("Game Name Here");
+		f.setSize(screenWidth, screenHeight);
+		f.setBackground(Color.BLACK);
+		f.setResizable(false);
+		f.addKeyListener(this);
+		f.addMouseMotionListener(this);
+		f.addMouseWheelListener(this);
+		f.addMouseListener(this);
+
+		f.add(this);
+
+		t = new Timer(15, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+
+		init();
+
+	}
+
+	Timer t;
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+		keys[e.getKeyCode()] = false;
+
+		if (keysToggled[e.getKeyCode()]) {
+			keysToggled[e.getKeyCode()] = false;
+		} else {
+			keysToggled[e.getKeyCode()] = true;
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		mouse[e.getButton()] = true;
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		mouse[e.getButton()] = false;
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		mouse[e.getButton()] = true;
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+
+	}
+
+}
